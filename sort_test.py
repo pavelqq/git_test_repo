e899852d77b9051ed4055381baa9df2293c2d554
Павел <pavel.schardakov@gmail.com>
2021-09-06 15:27:06 +0500

    feat(main) - add math

---
+++ sort_test.py
@@ -0,0 +1,31 @@
+from random import randint
+
+N = 10
+a = []
+for i in range(N):
+    a.append(randint(1, 99))
+print(a)
+
+
+for i in range(N-1):
+    for j in range(N-i-1):
+        if a[j] > a[j+1]:
+            a[j], a[j+1] = a[j+1], a[j]
+
+print(a)
+
+def insertion_sort(alist):
+    for i in range(1, len(alist)):
+        temp = alist[i]
+        j = i - 1
+        while (j >= 0 and temp < alist[j]):
+            alist[j + 1] = alist[j]
+            j = j - 1
+        alist[j + 1] = temp
+
+
+alist = input('Enter the list of numbers: ').split()
+alist = [int(x) for x in alist]
+insertion_sort(alist)
+print('Sorted list: ', end='')
+print(alist)
